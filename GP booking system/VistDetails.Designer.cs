ae6cc5e25b4ef6ab395808d92980d03b0bf28f4e
SashaCwl <147171591+SashaCwl@users.noreply.github.com>
2023-10-31 12:33:20 +0000

    Add files via upload

---
+++ GP booking system/VistDetails.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace GP_booking_system
+{
+    partial class VistDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.PatientFirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.PatientLastNameTextBox = new System.Windows.Forms.TextBox();
+            this.DoctorFirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.DoctorLastNameTextBox = new System.Windows.Forms.TextBox();
+            this.ReasonTextBox = new System.Windows.Forms.TextBox();
+            this.AddNotesTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.VerdictTextBox = new System.Windows.Forms.TextBox();
+            this.PerscriptionsTextBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(295, 27);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(117, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Visit Details";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Firstname";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lastname";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Firstname";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(217, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Lastname";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(138, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Patient";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(142, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Doctor";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // PatientFirstNameTextBox
+            // 
+            this.PatientFirstNameTextBox.Location = new System.Drawing.Point(13, 60);
+            this.PatientFirstNameTextBox.Name = "PatientFirstNameTextBox";
+            this.PatientFirstNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PatientFirstNameTextBox.TabIndex = 7;
+            this.PatientFirstNameTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // PatientLastNameTextBox
+            // 
+            this.PatientLastNameTextBox.Location = new System.Drawing.Point(220, 60);
+            this.PatientLastNameTextBox.Name = "PatientLastNameTextBox";
+            this.PatientLastNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PatientLastNameTextBox.TabIndex = 8;
+            // 
+            // DoctorFirstNameTextBox
+            // 
+            this.DoctorFirstNameTextBox.Location = new System.Drawing.Point(20, 60);
+            this.DoctorFirstNameTextBox.Name = "DoctorFirstNameTextBox";
+            this.DoctorFirstNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DoctorFirstNameTextBox.TabIndex = 9;
+            // 
+            // DoctorLastNameTextBox
+            // 
+            this.DoctorLastNameTextBox.Location = new System.Drawing.Point(216, 60);
+            this.DoctorLastNameTextBox.Name = "DoctorLastNameTextBox";
+            this.DoctorLastNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DoctorLastNameTextBox.TabIndex = 10;
+            // 
+            // ReasonTextBox
+            // 
+            this.ReasonTextBox.Location = new System.Drawing.Point(13, 151);
+            this.ReasonTextBox.Multiline = true;
+            this.ReasonTextBox.Name = "ReasonTextBox";
+            this.ReasonTextBox.Size = new System.Drawing.Size(307, 62);
+            this.ReasonTextBox.TabIndex = 11;
+            // 
+            // AddNotesTextBox
+            // 
+            this.AddNotesTextBox.Location = new System.Drawing.Point(19, 151);
+            this.AddNotesTextBox.Multiline = true;
+            this.AddNotesTextBox.Name = "AddNotesTextBox";
+            this.AddNotesTextBox.Size = new System.Drawing.Size(297, 62);
+            this.AddNotesTextBox.TabIndex = 12;
+            this.AddNotesTextBox.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(17, 135);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Additional Notes";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(10, 135);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(84, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Reason For Visit";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 406);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(113, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Perscriptions Assigned";
+            // 
+            // VerdictTextBox
+            // 
+            this.VerdictTextBox.Location = new System.Drawing.Point(12, 330);
+            this.VerdictTextBox.Multiline = true;
+            this.VerdictTextBox.Name = "VerdictTextBox";
+            this.VerdictTextBox.Size = new System.Drawing.Size(686, 58);
+            this.VerdictTextBox.TabIndex = 16;
+            // 
+            // PerscriptionsTextBox
+            // 
+            this.PerscriptionsTextBox.Location = new System.Drawing.Point(12, 422);
+            this.PerscriptionsTextBox.Multiline = true;
+            this.PerscriptionsTextBox.Name = "PerscriptionsTextBox";
+            this.PerscriptionsTextBox.Size = new System.Drawing.Size(686, 59);
+            this.PerscriptionsTextBox.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 314);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(40, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Verdict";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(12, 69);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.PatientFirstNameTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.PatientLastNameTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.ReasonTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.label9);
+            this.splitContainer1.Panel1.Controls.Add(this.label6);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label7);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.DoctorFirstNameTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.DoctorLastNameTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.label8);
+            this.splitContainer1.Panel2.Controls.Add(this.AddNotesTextBox);
+            this.splitContainer1.Size = new System.Drawing.Size(686, 228);
+            this.splitContainer1.SplitterDistance = 341;
+            this.splitContainer1.TabIndex = 19;
+            // 
+            // VistDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 506);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.PerscriptionsTextBox);
+            this.Controls.Add(this.VerdictTextBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label1);
+            this.Name = "VistDetails";
+            this.Text = "VistDetails";
+            this.Load += new System.EventHandler(this.VistDetails_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox PatientFirstNameTextBox;
+        private System.Windows.Forms.TextBox PatientLastNameTextBox;
+        private System.Windows.Forms.TextBox DoctorFirstNameTextBox;
+        private System.Windows.Forms.TextBox DoctorLastNameTextBox;
+        private System.Windows.Forms.TextBox ReasonTextBox;
+        private System.Windows.Forms.TextBox AddNotesTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox VerdictTextBox;
+        private System.Windows.Forms.TextBox PerscriptionsTextBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
