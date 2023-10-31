70d91718ef16c32fc6cbb67a0f99b566a63e4244
Sasha Cardwell <SC1640@canterbury.ac.uk>
2023-10-31 12:48:04 +0000

    visit details form

--- GP booking system/VisitDetails.cs
+++ GP booking system/VisitDetails.cs
@@ -16,5 +16,15 @@ namespace GP_booking_system
         {
             InitializeComponent();
         }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
