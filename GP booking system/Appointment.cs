b64299e7e9627655bc7654e51fc50c4faf061465
Pooja Rema <PR245@canterbury.ac.uk>
2023-10-31 12:02:43 +0000

    started to make the appointment class - pooja and rheana

---
+++ GP booking system/Appointment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GP_booking_system
+{
+    internal class Appointment
+    {
+        Patient_login mName;
+        DateTime mDate;
+        DateTime mTime;
+        String mDoctor;
+
+        #region Properties
+
+        public Patient_login Name {  get { return mName; } }
+
+        public DateTime Date { get { return mDate; } set { mDate = value; } }
+
+        public DateTime Time { get { return mTime; } set { mTime = value; } }
+
+        public String Doctor { get {  return mDoctor; } set { mDoctor = value; } }
+
+        #endregion
+
+        #region Constructors
+
+        
+        public Appointment(DateTime date, DateTime time)
+        {
+           mDate = date;
+           mTime = time;
+
+        }
+
+
+        #endregion
+    }
+}
